5f6a8b1f2d161d5f0785a6261526df36b814a835
Shamah M Zoha <shamah.zoha@gmail.com>
2022-04-29 21:49:28 +0600

    Added AppRunner class to run application

---
+++ src/main/java/com/zedapps/bookshare/AppRunner.java
@@ -0,0 +1,17 @@
+package com.zedapps.bookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shamah M Zoha
+ * @since 29-Apr-22
+ */
+
+@SpringBootApplication
+public class AppRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRunner.class, args);
+    }
+}
